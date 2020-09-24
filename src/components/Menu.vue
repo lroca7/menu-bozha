dea5a57f2895b9db02c5e433862493f29734bf17
lizeth <lizethcabrales@gmail.com>
2020-09-24 13:26:29 -0500

    menu bozha test

---
+++ src/components/Menu.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+    <p>
+        Nuestro Menu
+    </p>
+    <img src='../assets/1.png' />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Menu',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
